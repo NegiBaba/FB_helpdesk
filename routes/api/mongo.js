cc70fcef126c85dfd6fb0f2b36ac0e95c3f742cf
NegiBaba <piyushnegi319@gmail.com>
2021-09-12 22:24:18 +0530

    testing

--- routes/api/mongo.js
+++ routes/api/mongo.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-console.log(process.env.MONGO_URI);
+console.log(process.env);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
