e68fd1dc77e1d4ba44e57d38d2e40bf8dfa5be10
NegiBaba <piyushnegi319@gmail.com>
2021-09-12 22:14:53 +0530

    testing: :

--- routes/api/mongo.js
+++ routes/api/mongo.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
 .then(() => console.log("connected to Mongodb"))
 .catch((err) => console.log("cannot connect to mongo server"));
 
