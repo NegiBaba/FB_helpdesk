84c0fca0060a51ff0800a96c653cd4c4be6f9571
NegiBaba <piyushnegi319@gmail.com>
2021-09-12 22:11:25 +0530

    testing

--- routes/api/mongo.js
+++ routes/api/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("connected to Mongodb"));
+.then(() => console.log("connected to Mongodb"))
+.catch((err) => console.log("cannot connect to mongo server"));
 
 module.exports = mongoose;
