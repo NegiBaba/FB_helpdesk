13da238b56f670b1799be13640789210b1055ffb
NegiBaba <piyushnegi319@gmail.com>
2021-09-14 00:26:42 +0530

    added small changes

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,3 +1,9 @@
+const request = require('request');
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
+    
 app.post('/user', async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     console.log("body is this - ", req.body);
