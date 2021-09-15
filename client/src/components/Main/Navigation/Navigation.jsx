3b7d30c75879b013dc328b7b171ae07be4f3cd35
NegiBaba <piyushnegi319@gmail.com>
2021-09-15 12:02:37 +0530

    added icons for main page

--- client/src/components/Main/Navigation/Navigation.jsx
+++ client/src/components/Main/Navigation/Navigation.jsx
@@ -1,6 +1,9 @@
 import inbox from "./Logo/inbox.svg";
+import inbox_base from "./Logo/inbox_white.svg";
 import people from "./Logo/people.svg";
+import people_base from "./Logo/people_white.svg";
 import facebook from "./Logo/facebook.svg";
+import logout_white from "./Logo/logout_white.svg";
 
 import "./Navigation.css";
 export default function Navigation() {
@@ -8,9 +11,9 @@ export default function Navigation() {
         <div className='navbar'>
             <img className="nav_logo" src={facebook} />
             <ul className='nav-list'>
-                <li className="nav-item"><img src={inbox} alt="inbox" /></li>
-                <li className="nav-item"><img src={people} alt="people" /> </li>
-                <li className="nav-item">logout</li>
+                <li className="nav-item"><img src={inbox_base} alt="inbox" /></li>
+                <li className="nav-item"><img src={people_base} alt="people" /> </li>
+                <li className="nav-item"><img src={logout_white} alt='logout' /></li>
             </ul>
         </div>
     );
